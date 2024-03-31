4c72530f6772453494e763541834dee35245fbac
kaiser <chidorixzx@gmail.com>
2024-03-31 14:16:24 +0700

    + serviceBUS + serviceDTO

---
+++ src/DTO/serviceDTO.java
@@ -0,0 +1,32 @@
+package DTO;
+
+public class serviceDTO {
+    private int service_id;
+    private String service_name;
+    private String service_price;
+
+    public int getService_id() {
+        return service_id;
+    }
+
+    public void setService_id(int service_id) {
+        this.service_id = service_id;
+    }
+
+    public String getService_name() {
+        return service_name;
+    }
+
+    public void setService_name(String service_name) {
+        this.service_name = service_name;
+    }
+
+    public String getService_price() {
+        return service_price;
+    }
+
+    public void setService_price(String service_price) {
+        this.service_price = service_price;
+    }
+
+}
